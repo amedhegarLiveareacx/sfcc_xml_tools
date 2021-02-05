c3ab12b1cc4d5357571bc17a8aec7f940f0b3528
Amasidda <amedhegar@pfsweb.com>
2021-02-05 07:15:06 +0530

    added the utility methods

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-// var fs = require('fs');
-// var parser = require('xml2json');
-
-// var productPricePair = {};
-
-// fs.readFile( './test_pricebook.xml', 'utf8', function(err, xmlData) {
-//     var json = JSON.parse(parser.toJson(xmlData, {reversible: true}));
-//     var priceTables = json.pricebooks.pricebook['price-tables']['price-table'];
-
-//     priceTables.forEach(element => {
-//         productPricePair[element['product-id']] = element.amount['$t'];
-//     });
-//     console.log(productPricePair);
-// });
-
 const pricebook = require('./pricebook');
 
-const productPricePair = pricebook.getProductIdPricePair();
-console.log(productPricePair);
+// const productPricePair = pricebook.getProductIdPricePair();
+// console.log(productPricePair['193445206028']);
+
+// const catalogProductPair = pricebook.getCatalogProductList();
+
+pricebook.generateCatalogProductPair();
+// pricebook.generateProductPricePair();
 
 
 
--- pricebook.js
+++ pricebook.js
@@ -3,8 +3,8 @@ var parser = require('xml2json');
 
 class PriceBookParser {
     constructor() {
-        this.pricebookFileName = './test_pricebook.xml';
-        this.catalogPriceBook = '';
+        this.pricebookFileName = './pricebooks/usd_list/xaa.xml'; // './test_pricebook.xml';
+        this.catalogPriceBook = './catalogs/dev08_NA_master_catalog.xml'; // './test_catalog.xml';
     }
     getProductIdPricePair() {
         var productPricePair = {};
@@ -19,10 +19,41 @@ class PriceBookParser {
         return productPricePair;
     }
 
-    // getCatalogProductList(filename) {
+    generateCatalogProductPair() {
+        var catalogProductPair = {};
+        var xmlData = fs.readFileSync(this.catalogPriceBook, 'utf8');
+        if (xmlData) {
+            var json = JSON.parse(parser.toJson(xmlData, {reversible: true}));
+            json.catalog.product.forEach(element => {
+                catalogProductPair[element['product-id']] = 0;
+            });            
+        }
+        this.writeCatalogPriceBookToFile(catalogProductPair);
+    }
 
-    // }
+    readCatalogPriceBookFromFile() {
+        var data = fs.readFileSync('pricebookpair.json', 'utf8');
+        return data ? JSON.parse(data) : [];
+    }
+
+    writeCatalogPriceBookToFile(records) {
+        fs.writeFileSync('pricebookpair.json', JSON.stringify(records));
+    }
+
+    generateProductPricePair() {
+        const productPricePair = this.getProductIdPricePair();
+        const catalogProductPair = this.readCatalogPriceBookFromFile();
+
+        for (const key in catalogProductPair) {
+            if (Object.hasOwnProperty.call(catalogProductPair, key)) {
+                // const element = catalogProductPair[key];
+                if (productPricePair[key]) {
+                    catalogProductPair[key] = productPricePair[key];
+                }
+            }
+        }
+        this.writeCatalogPriceBookToFile(catalogProductPair);
+    }
 }
-
 
 module.exports = new PriceBookParser();
