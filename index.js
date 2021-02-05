d7ca179b2c7321a06453385a14b7f41f5d920769
Amasidda <amedhegar@pfsweb.com>
2021-02-05 09:56:08 +0530

    Code changes to update the output file path

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ pricebook.generateCatalogProductPair('usd_list');
 pricebook.generateProductPricePair('usd_list');
 pricebook.generatePricebookXml('usd_list');
 
-
 pricebook.generateCatalogProductPair('usd_sale');
 pricebook.generateProductPricePair('usd_sale');
 pricebook.generatePricebookXml('usd_sale');
--- pricebook.js
+++ pricebook.js
@@ -1,95 +1,127 @@
 var fs = require('fs');
 const path = require('path');
 var parser = require('xml2json');
-
 class PriceBookParser {
-    constructor() {
-        this.catalogPriceBook = './catalogs/dev08_NA_master_catalog.xml'; // './test_catalog.xml';
+  constructor() {
+    this.catalogPriceBook = './catalogs/dev08_NA_master_catalog.xml'; // './test_catalog.xml';
+  }
+  // This method generate key value pair from the pricebooks file
+  getProductIdPricePair(filename) {
+    var productPricePair = {};
+    var xmlData = fs.readFileSync(filename, 'utf8');
+    if (xmlData) {
+      var json = JSON.parse(parser.toJson(xmlData, { reversible: true }));
+      var priceTables =
+        json.pricebooks.pricebook['price-tables']['price-table'];
+      priceTables.forEach((element) => {
+        productPricePair[element['product-id']] = element.amount['$t'];
+      });
     }
-    // This method generate key value pair from the pricebooks file
-    getProductIdPricePair(filename) {
-        var productPricePair = {};
-        var xmlData = fs.readFileSync(filename, 'utf8');
-        if (xmlData) {
-            var json = JSON.parse(parser.toJson(xmlData, { reversible: true }));
-            var priceTables = json.pricebooks.pricebook['price-tables']['price-table'];
-            priceTables.forEach(element => {
-                productPricePair[element['product-id']] = element.amount['$t'];
-            });
-        }
-        return productPricePair;
+    return productPricePair;
+  }
+
+  // This method generate key value pair from the catalog file
+  generateCatalogProductPair(priceBookType) {
+    console.log(
+      '=====================Generating catalog product pair=============='
+    );
+    var catalogProductPair = {};
+    const filename = path.join(
+      __dirname,
+      'generatedfiles',
+      'pricebooks',
+      priceBookType + '_pricebookpair.json'
+    ); // priceBookType + '_pricebookpair.json';
+    var xmlData = fs.readFileSync(this.catalogPriceBook, 'utf8');
+    if (xmlData) {
+      var json = JSON.parse(parser.toJson(xmlData, { reversible: true }));
+      json.catalog.product.forEach((element) => {
+        catalogProductPair[element['product-id']] = 0;
+      });
     }
+    this.writeDataToFile(catalogProductPair, filename);
+    console.log(
+      '=====================Finished generating catalog product pair=============='
+    );
+  }
 
-    // This method generate key value pair from the catalog file
-    generateCatalogProductPair(priceBookType) {
-        console.log("=====================Generating catalog product pair==============");
-        var catalogProductPair = {};
-        const filename = priceBookType + '_pricebookpair.json';
-        var xmlData = fs.readFileSync(this.catalogPriceBook, 'utf8');
-        if (xmlData) {
-            var json = JSON.parse(parser.toJson(xmlData, { reversible: true }));
-            json.catalog.product.forEach(element => {
-                catalogProductPair[element['product-id']] = 0;
-            });
-        }
-        this.writeDataToFile(catalogProductPair, filename);
-        console.log("=====================Finished generating catalog product pair==============");
-    }
+  // Read the data from the file
+  readCatalogPriceBookFromFile(priceBookType) {
+    const filename = path.join(
+      __dirname,
+      'generatedfiles',
+      'pricebooks',
+      priceBookType + '_pricebookpair.json'
+    );
+    var data = fs.readFileSync(filename, 'utf8');
+    return data ? JSON.parse(data) : [];
+  }
 
-    // Read the data from the file
-    readCatalogPriceBookFromFile(priceBookType) {
-        const filename = priceBookType + '_pricebookpair.json';
-        var data = fs.readFileSync(filename, 'utf8');
-        return data ? JSON.parse(data) : [];
-    }
+  // write the data to file
+  writeDataToFile(records, filename) {
+    fs.writeFileSync(filename, JSON.stringify(records));
+  }
 
-    // write the data to file
-    writeDataToFile(records, filename) {
-        fs.writeFileSync(filename, JSON.stringify(records));
-    }
-
-    // This method sets the corresponding price to the key value pair and write the data back to file
-    generateProductPricePair(priceBookType) {
-        const priceBookPairFileName = priceBookType + '_pricebookpair.json';
-        const directoryPath = path.join(__dirname, 'pricebooks', priceBookType);
-        const filenames = fs.readdirSync(directoryPath);
-        if (filenames) {
-            filenames.forEach((filename) => {
-                const catalogProductPair = this.readCatalogPriceBookFromFile(priceBookType);
-                console.log(`===============Reading the file:: ${filename} ===================`)
-                const productPricePair = this.getProductIdPricePair(path.join(directoryPath, filename));
-                for (const key in catalogProductPair) {
-                    if (Object.hasOwnProperty.call(catalogProductPair, key)) {
-                        // const element = catalogProductPair[key];
-                        if (productPricePair[key]) {
-                            catalogProductPair[key] = productPricePair[key];
-                        }
-                    }
-                }
-                this.writeDataToFile(catalogProductPair, priceBookPairFileName);
-                console.log(`===============Finished reading the file:: ${filename} ===================`);
-            });
-        }
-    }
-
-    generatePricebookXml(priceBookType) {
-        const catalogProductPair = this.readCatalogPriceBookFromFile(priceBookType);
-        const finalPriceBookFileName = priceBookType === 'usd_list' ? 'USD-list.xml' : 'USD-sale.xml';
-        var priceXml = '';
-        if (priceBookType === 'usd_list') {
-            priceXml += `<?xml version="1.0" encoding="UTF-8"?><pricebooks xmlns="http://www.demandware.com/xml/impex/pricebook/2006-10-31"><pricebook><header pricebook-id="USD-list"><currency>USD</currency><online-flag>true</online-flag><feed-based>true</feed-based></header><price-tables>`;
-        } else {
-            priceXml += `<?xml version="1.0" encoding="UTF-8"?><pricebooks xmlns="http://www.demandware.com/xml/impex/pricebook/2006-10-31"><pricebook><header pricebook-id="USD-sale"><currency>USD</currency><online-flag>true</online-flag><parent>USD-list</parent><feed-based>true</feed-based></header><price-tables>`;
-        }
-
+  // This method sets the corresponding price to the key value pair and write the data back to file
+  generateProductPricePair(priceBookType) {
+    const priceBookPairFileName = path.join(
+      __dirname,
+      'generatedfiles',
+      'pricebooks',
+      priceBookType + '_pricebookpair.json'
+    ); // priceBookType + '_pricebookpair.json';
+    const directoryPath = path.join(__dirname, 'pricebooks', priceBookType);
+    const filenames = fs.readdirSync(directoryPath);
+    if (filenames) {
+      filenames.forEach((filename) => {
+        const catalogProductPair = this.readCatalogPriceBookFromFile(
+          priceBookType
+        );
+        console.log(
+          `===============Reading the file:: ${filename} ===================`
+        );
+        const productPricePair = this.getProductIdPricePair(
+          path.join(directoryPath, filename)
+        );
         for (const key in catalogProductPair) {
-            if (Object.hasOwnProperty.call(catalogProductPair, key)) {
-                priceXml += `<price-table product-id="${key}"><amount quantity="1">${catalogProductPair[key]}</amount></price-table>`
+          if (Object.hasOwnProperty.call(catalogProductPair, key)) {
+            if (productPricePair[key]) {
+              catalogProductPair[key] = productPricePair[key];
             }
+          }
         }
-        priceXml += `</price-tables></pricebook></pricebooks>`;
-        this.writeDataToFile(priceXml, finalPriceBookFileName);
+        this.writeDataToFile(catalogProductPair, priceBookPairFileName);
+        console.log(
+          `===============Finished reading the file:: ${filename} ===================`
+        );
+      });
     }
+  }
+
+  generatePricebookXml(priceBookType) {
+    const catalogProductPair = this.readCatalogPriceBookFromFile(priceBookType);
+    const finalPriceBookFileName =
+      priceBookType === 'usd_list' ? 'USD-list.xml' : 'USD-sale.xml';
+    const outPutFile = path.join(
+      __dirname,
+      'generatedfiles',
+      'pricebooks',
+      finalPriceBookFileName
+    );
+    var priceXml = '';
+    if (priceBookType === 'usd_list') {
+      priceXml += `<?xml version="1.0" encoding="UTF-8"?><pricebooks xmlns="http://www.demandware.com/xml/impex/pricebook/2006-10-31"><pricebook><header pricebook-id="USD-list"><currency>USD</currency><online-flag>true</online-flag><feed-based>true</feed-based></header><price-tables>`;
+    } else {
+      priceXml += `<?xml version="1.0" encoding="UTF-8"?><pricebooks xmlns="http://www.demandware.com/xml/impex/pricebook/2006-10-31"><pricebook><header pricebook-id="USD-sale"><currency>USD</currency><online-flag>true</online-flag><parent>USD-list</parent><feed-based>true</feed-based></header><price-tables>`;
+    }
+    for (const key in catalogProductPair) {
+      if (Object.hasOwnProperty.call(catalogProductPair, key)) {
+        priceXml += `<price-table product-id="${key}"><amount quantity="1">${catalogProductPair[key]}</amount></price-table>`;
+      }
+    }
+    priceXml += `</price-tables></pricebook></pricebooks>`;
+    this.writeDataToFile(priceXml, outPutFile);
+  }
 }
 
 module.exports = new PriceBookParser();
